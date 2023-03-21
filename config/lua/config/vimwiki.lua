e9893387e1adf161a4a5ce8d63629bb41a895a73
9glenda <plan9git@proton.me>
2023-03-21 16:46:28 +0000

    vimwiki

--- config/lua/config/vimwiki.lua
+++ config/lua/config/vimwiki.lua
@@ -2,4 +2,24 @@
 --if not vimwiki_status_ok then
 --  return
 --end
-vim.g.vimwiki_list = {{path = '~/vimwiki/', syntax = 'markdown', ext = '.md'}}
+vim.g.vimwiki_list = {
+  {
+    path = "/opt/code/wiki/",
+    index = "main",
+    syntax = "markdown",
+    ext = "md",
+    auto_diary_index = 1,
+    auto_toc = 1,
+    auto_generte_links = 1
+  }
+}
+
+vim.g.vimwiki_ext2syntax = {
+  ['.md'] = 'markdown',
+  ['.markdown'] = 'markdown',
+  ['.mdown'] = 'markdown',
+}
+
+vim.g.vimwiki_markdown_link_ext = 1
+vim.g.taskwiki_markup_syntax = 'markdown'
+vim.g.markdown_folding = 1
